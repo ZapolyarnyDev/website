5001f019a516fa0f616e8576a01a01ba001e0ec8
Zapolyarny <zapolyarnynorth@gmail.com>
2026-04-06 21:30:19 +0300

    style: format project files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 
 export default defineConfig({
-  integrations: [mdx(), sitemap()]
+  integrations: [mdx(), sitemap()],
 });
